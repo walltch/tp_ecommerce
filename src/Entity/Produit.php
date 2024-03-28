6abf7d27954639e73f4bbf2f080a7dd1bf0d29a4
mrazakandraina <mrazakandraina@optilian.com>
2024-03-28 10:42:07 +0100

    redirection de la page d'accueil sur produit

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
 class Produit
 {
@@ -97,7 +98,7 @@ class Produit
         return $this->photo;
     }
 
-    public function setPhoto(string $photo): static
+    public function setPhoto(?string $photo): static
     {
         $this->photo = $photo;
 
