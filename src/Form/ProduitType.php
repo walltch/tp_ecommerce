d29407342449160c7e9d33816607b2f46e03ef19
walltch <walltch@github.com>
2024-03-28 11:45:39 +0100

    Product btn enregistrer

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -5,11 +5,8 @@ namespace App\Form;
 use App\Entity\Produit;
 use PHPUnit\TextUI\XmlConfiguration\File;
 use Symfony\Component\Form\AbstractType;
-<<<<<<< Updated upstream
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-=======
 use Symfony\Component\Form\Extension\Core\Type\FileType;
->>>>>>> Stashed changes
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,19 +20,14 @@ class ProduitType extends AbstractType
             ->add('description')
             ->add('prix')
             ->add('stock')
-<<<<<<< Updated upstream
-            ->add('photo')
-            ->add('save', SubmitType::class, [
-                'label' => $options['button_label'], 
-            ]);
-=======
             ->add('photo', FileType::class, [
                 'label' => 'Image (jpg, png)',
                 'mapped' => false,
                 'required' => false,
             ])
-
->>>>>>> Stashed changes
+            ->add('save', SubmitType::class, [
+                'label' => $options['button_label'],
+            ])
         ;
     }
 
