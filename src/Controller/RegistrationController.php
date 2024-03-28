40f5d96a0945dd8950632e7282e031446d398dd3
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2024-03-28 08:53:04 +0100

    correction du champ role initialement à null

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,7 +30,8 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+    
+            $user->setRoles(['ROLE_USER']);
             $entityManager->persist($user);
             $entityManager->flush();
 
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,7 +17,7 @@ class UserController extends AbstractController
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('user/index.html.twig', [  
             'users' => $userRepository->findAll(),
         ]);
     }
