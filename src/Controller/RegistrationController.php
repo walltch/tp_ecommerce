81b9b7a76c515085c858d9660d64b7799775f8cc
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2024-03-29 16:11:51 +0100

    super admin

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -74,4 +74,34 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form,
         ]);
     }
+
+    #[Route('/register/superadmin', name: 'app_register_admin')]
+    public function registerSuperAdmin(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+    
+            $user->setRoles(['ROLE_SUPER_ADMIN']);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $security->login($user, AuthAuthenticator::class, 'main');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
 }
